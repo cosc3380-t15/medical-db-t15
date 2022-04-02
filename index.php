0d420afe989862f5501930dac2302242425d36d1
eyiece-krheme <msasummers@yahoo.com>
2022-04-02 11:36:28 -0500

    mysql

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php header('Location: index/home.html'); ?>
+<?php
+$servername = "eyiece.mynetgear.com";
+$username = "root";
+$password = "b01r18o11A!ae&";
+$database = "medical_db";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
