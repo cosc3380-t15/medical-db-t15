62e321fc96044b43531842239850b38ecad652a3
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-02 21:13:52 -0500

    drag

--- index/patient.php
+++ index/patient.php
@@ -26,22 +26,7 @@ header('Location: index/home.html');
 </head>
 
 <body>
-    <h1>
-        <?php
-        $sql = "SELECT * FROM doctor;";
-        $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result);
-
-        if ($resultCheck > 0){
-            while ($row = mysqli_fetch_assoc($result)){
-                echo $row['Doc_First'];
-            }    
-        }
-
-
-    ?>
-    </h1>
-
+<h1><?php echo "This message is from server side." ?></h1>
 
 
 
