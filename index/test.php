9b036fb67f121ccefa11625eff30fb3494f0e978
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-02 21:53:23 -0500

    drago

---
+++ index/test.php
@@ -0,0 +1,13 @@
+<?php
+include '/index.php'
+$sql = "SELECT * FROM doctor;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0){
+    while ($row = mysqli_fetch_assoc($result)){
+        echo $row['Doc_Spec'];
+    }    
+}
+
+?>
