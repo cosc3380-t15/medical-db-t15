d96fd3a373617baa8799a8f4b4bddb842ba14607
eyiece-krheme <msasummers@yahoo.com>
2022-04-03 17:14:18 -0500

    connect pls

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $cleardb_db = "medical_db";
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db, 3306, "mysql2");
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
- echo "Connected successfully";
+echo "Connected successfully";
 ?>
