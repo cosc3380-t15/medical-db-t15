8cef748b01c23c363035e17e188130d640ddab83
eyiece-krheme <msasummers@yahoo.com>
2022-04-03 16:44:01 -0500

    connnect

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 <?php
-$servername = "eyiece.mynetgear.com";
-$username = "DNonov";
-$password = "D27m03r94!@#";
-$database = "medical_db";
-// Create connection
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
 
-try{
-  $con = new PDO("mysql:host=$servername;dbname=$database",$username, $password);
-
-  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connection Success";
-}
-catch (PDOException $e){
-  echo "Error is connection " . $e->getMessage();
-}
-
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
