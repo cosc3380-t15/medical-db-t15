74bbaa35967ec6124a12916eb8ecdd2f1d422ecc
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-03 21:28:04 -0500

    drago

--- index/test.php
+++ index/test.php
@@ -1,16 +1,16 @@
 <!-- <?php
+ include "/backend/connect.php";
 
-        include "/backend/connect.php";
+ $query1 = "INSERT INTO patient (Pat_ID, Pat_First, Pat_last,Pat_M_init,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr)
+ VALUES (1005, "drago", "nonov","r","qwdasds@gmail.vom","234234456",2,2,"1990-12-11",211,344,"123 asder","houston","texas",77077);";
 
-        $sql = "SELECT * FROM doctor;";
-        $result = mysqli_query($conn, $sql);
-        $resultCheck = mysqli_num_rows($result);
+ if (mysqli_query($conn, $query1)) {
+     # code...
+     echo "<script type = 'text/javastript'>alert('Success')</script>";
 
-        if ($resultCheck > 0){
-            while ($row = mysqli_fetch_assoc($result)){
-                echo $row['Doc_Spec'];
-            }    
-        }
-
+ }else {
+     echo "<script type = 'text/javastript'>alert('Failure')</script>";
+ }
+ mysqli_close($conn);
 
     ?>
