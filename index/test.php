d1cd900a59542faaddd6cb4a2a346a85357209fa
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-03 21:33:55 -0500

    drago

--- index/test.php
+++ index/test.php
@@ -8,18 +8,15 @@ $dbname = getenv("DBNAME");
 $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
 mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($link, $test_query);
+$query1 = "INSERT INTO patient (Pat_ID, Pat_First, Pat_last,Pat_M_init,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr)
+VALUES (1005, "drago", "nonov","r","qwdasds@gmail.vom","234234456",2,2,"1990-12-11",211,344,"123 asder","houston","texas",77077);";
 
-$tblCnt = 0;
-while($tbl = mysqli_fetch_array($result)) {
-  $tblCnt++;
-  echo $tbl[0]."<br />\n";
-}
+if (mysqli_query($conn, $query1)) {
+    # code...
+    echo "<script type = 'text/javastript'>alert('Success')</script>";
 
-if (!$tblCnt) {
-  echo "There are no tables<br />\n";
-} else {
-  echo "There are $tblCnt tables<br />\n";
+}else {
+    echo "<script type = 'text/javastript'>alert('Failure')</script>";
 }
+mysqli_close($conn);
 ?>
