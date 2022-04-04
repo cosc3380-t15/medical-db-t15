c839a7d83eb52f39c4785bf6f7bd144c32e0a67d
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-03 22:07:29 -0500

    drago

--- index/test.php
+++ index/test.php
@@ -10,7 +10,7 @@ mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
 $query = "SELECT * FROM medical_db.patient";
 $query1 = "INSERT INTO patient (Pat_ID, Pat_First, Pat_last,Pat_M_init,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr)
-VALUES (1005,"john","nonov","r","qwdasds@gmail.vom","234234456",2,2,"1990-12-11",211,344,"123 asder","houston","texas",77077)";
+VALUES (1005,'john','nonov','r','qwdasds@gmail.vom','234234456',2,2,'1990-12-11',211,344,'123 asder','houston','texas',77077)";
 
 $result = mysqli_query($link, $query1);
 
