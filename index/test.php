487a2155e18bc0371997ceec3b222975ac553146
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-03 22:09:13 -0500

    drago

--- index/test.php
+++ index/test.php
@@ -12,13 +12,6 @@ $query = "SELECT * FROM medical_db.patient";
 $query1 = "INSERT INTO patient (Pat_ID, Pat_First, Pat_last,Pat_M_init,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr)
 VALUES (1005,'john','nonov','r','qwdasds@gmail.vom','234234456',2,2,'1990-12-11',211,344,'123 asder','houston','texas',77077)";
 
-$result = mysqli_query($link, $query1);
-
-if ($result) {
-    echo 'Data Inserted'
-}else{
-    echo 'Data Not Inserted'
-}
 
 mysqli_free_result($result);
 mysqli_close($link)
