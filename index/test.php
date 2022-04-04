60d78ae83f69c82f9498a131d069751986beba77
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-03 22:19:58 -0500

    drago

--- index/test.php
+++ index/test.php
@@ -10,10 +10,14 @@ mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
 // $query = "SELECT * FROM medical_db.patient";
 $query1 = "INSERT INTO patient (Pat_ID, Pat_First, Pat_last,Pat_M_init,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr)
-VALUES (3333,'john','nonov','r','qwdasds@gmail.vom','234234456',2,2,'1990-12-11',211,344,'123 asder','houston','texas',77077)";
+VALUES (5555,'john','nonov','r','qwdasds@gmail.vom','234234456',2,2,'1990-12-11',211,344,'123 asder','houston','texas',77077)";
 
 
-
+if ($conn->query($query1) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $query1 . "<br>" . $conn->error;
+  }
 mysqli_close($link)
 
 ?>
