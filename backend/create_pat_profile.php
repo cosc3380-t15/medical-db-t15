b6bfaeeab93bfab189c581e9751c03c67be4ea84
eyiece-krheme <msasummers@yahoo.com>
2022-04-06 05:17:10 -0500

    add password

--- backend/create_pat_profile.php
+++ backend/create_pat_profile.php
@@ -15,6 +15,7 @@ $city = $_POST['city'];
 $state = $_POST['state'];
 $zip = $_POST['zip'];
 $allergy = $_POST['allergies'];
+$password = $_POST['password'];
 
 
                                                         # code...
@@ -27,8 +28,8 @@ $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '
 mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
                                                         
-$query1 = "INSERT INTO patient (Pat_First,Pat_M_init,Pat_last,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr,Pat_Allergy)
-VALUES ('$firstname','$minit','$lname','$email','$phone','$gender','$race','$dob','$height','$weight','$address','$city','$state','$zip','$allergy')";
+$query1 = "INSERT INTO patient (Pat_First,Pat_M_init,Pat_last,Pat_Email,Pat_Phone,Pat_Gender,Pat_Race,Pat_DOB,Pat_Height,Pat_Weight,Pat_Street_Addr,Pat_City_Addr,Pat_State_Addr,Pat_Zip_Addr,Pat_Allergy,Pat_Password)
+VALUES ('$firstname','$minit','$lname','$email','$phone','$gender','$race','$dob','$height','$weight','$address','$city','$state','$zip','$allergy','$password')";
 
 
 if ($link->query($query1) !== TRUE) {
