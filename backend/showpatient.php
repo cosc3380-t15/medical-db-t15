d3fe8836752a920413ee2e762f46ec726a14e38f
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-05 20:22:38 -0500

    dr

---
+++ backend/showpatient.php
@@ -0,0 +1,25 @@
+<?php
+                                                        # code...
+$dbhost = getenv("DBHOST");
+$dbuser = getenv("DBUSER");
+$dbpass = getenv("DBPASS"); 
+$dbname = getenv("DBNAME");
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+                                                        
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+
+$sql = "SELECT Pat_ID, Pat_First, Pat_Last, Pat_Email, Pat_Phone FROM patient";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
+  $link->close();
+
+?>
