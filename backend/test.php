2047b3eba61a651f37a32a9fe32492811cfdd675
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-05 22:59:54 -0500

    dd

--- backend/test.php
+++ backend/test.php
@@ -22,3 +22,28 @@ mysqli_close($link)
 
 ?>
 
+<?php
+                                                        # code...
+$dbhost = getenv("DBHOST");
+$dbuser = getenv("DBUSER");
+$dbpass = getenv("DBPASS"); 
+$dbname = getenv("DBNAME");
+$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+                                                        
+mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+
+$sql = "SELECT Pat_ID, Pat_First, Pat_Last, Pat_Email, Pat_Phone FROM patient";
+$result = $link->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "Patient ID:  " . $row["Pat_ID"]. "  Name: " . $row["Pat_First"]. " " . $row["Pat_Last"]. "  Email  " . $row["Pat_Email"]. "  Phone  " . $row["Pat_Phone"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
+  $link->close();
+
+?>
