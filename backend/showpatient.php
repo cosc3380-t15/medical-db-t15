a882aeebf9b14fc6818a7f3f6567df3cdba2a067
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-05 22:21:21 -0500

    dd

--- backend/showpatient.php
+++ backend/showpatient.php
@@ -37,40 +37,62 @@ if ($result->num_rows > 0) {
 <div class="card">
     
         <a href="/index/patient.html"><input class="button" type="submit" value="Back"></a>     
-        <?php
-                                                        # code...
-$dbhost = getenv("DBHOST");
-$dbuser = getenv("DBUSER");
-$dbpass = getenv("DBPASS"); 
-$dbname = getenv("DBNAME");
-$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+        <?php                                                    # code...
+    $dbhost = getenv("DBHOST");
+    $dbuser = getenv("DBUSER");
+    $dbpass = getenv("DBPASS"); 
+    $dbname = getenv("DBNAME");
+    $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
                                                         
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
-
-$sql = "SELECT Pat_ID, Pat_First, Pat_Last, Pat_Email, Pat_Phone FROM patient";
-$result = $link->query($sql);
-
-
-
-if($result){ // only execute this if there are results ?>
-<div>
+    mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+    $sql = "SELECT Pat_ID, Pat_First, Pat_Last, Pat_Email, Pat_Phone FROM patient";
+    $result = $link->query($sql);
+    if($result){ // only execute this if there are results ?>
+    <div class="container">
+        <h4>Patient ID:</h2>
       <ul> 
-    <?php
-    $count = 0;
-    foreach($result as $row){ //loop over all the results?>
-    <h4>Patient ID:</h2>
+        <?php
+        $count = 0;
+        foreach($result as $row){ //loop over all the results?>
        <li class="<?php // if this is the first row output the first-row class, 
                        // otherwise output other-row class
            echo $count==0 ? 'first-row' : 'other-row'; ?>">
            <?php echo $row["Pat_ID"]; ?><button class="float-right">Delete</button></li>
-<?php $count++; // increment my count var
-    } // endforeach?>
-   </ul>
-</div>
+        <?php $count++; // increment my count var
+        } // endforeach?>
+        </ul>
+    </div>
+    <div class="container">
+        <h4>Name:</h2>
+      <ul> 
+        <?php
+        $count = 0;
+        foreach($result as $row){ //loop over all the results?>
+       <li class="<?php // if this is the first row output the first-row class, 
+                       // otherwise output other-row class
+           echo $count==0 ? 'first-row' : 'other-row'; ?>">
+           <?php echo $row["Pat_First"]; ?><button class="float-right">Delete</button></li>
+        <?php $count++; // increment my count var
+        } // endforeach?>
+        </ul>
+    </div>
+    <div class="container">
+      <ul> 
+        <?php
+        $count = 0;
+        foreach($result as $row){ //loop over all the results?>
+       <li class="<?php // if this is the first row output the first-row class, 
+                       // otherwise output other-row class
+           echo $count==0 ? 'first-row' : 'other-row'; ?>">
+           <?php echo $row["Pat_Last"]; ?><button class="float-right">Delete</button></li>
+        <?php $count++; // increment my count var
+        } // endforeach?>
+        </ul>
+    </div>
   
 
-<?php 
-} //end if?>
+    <?php 
+    } //end if?>
 
     
 </div>
