fa0e97bbb5d2f02dbc45af858e84f314f50c2fec
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-08 00:27:12 -0500

    dr

---
+++ backend/edit_patient.php
@@ -0,0 +1,126 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <link rel="stylesheet" href="/index/styles/form.css">
+</head>
+
+<body>
+<?php
+if (isset($_SESSION['status'])) {
+    echo "<h4>".$_SESSION['status']."</h4>";
+    unset($_SESSION['status']);
+}
+?>
+    <div class="container-form11">
+        <form action="/backend/update_patient_data.php" method="post" onsubmit="return Validate();">
+            <div class="row">
+                <h4>Update Patient Information</h4>
+                <div class="input-group input-group-icon">
+                    <input type="text" placeholder="Patient ID" name="pat_id" required />
+                    <div class="input-icon"><i class="fa fa-user"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input type="text" placeholder="First Name" name="fname" required />
+                    <div class="input-icon"><i class="fa fa-user"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input type="text" placeholder="Middle Name" name="minit" />
+                    <div class="input-icon"><i class="fa fa-user"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input type="text" placeholder="Last Name" name="lname" required />
+                    <div class="input-icon"><i class="fa fa-user"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input type="email" placeholder="Email Adress" name="email" required />
+                    <div class="input-icon"><i class="fa fa-envelope"></i></div>
+                </div>
+                <div class="input-group input-group-icon">
+                    <input type="text" placeholder="Phone Number" name="phone" required />
+                    <div class="input-icon"><i class="fa fa-key"></i></div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-group input-group-icon">
+                    <label for="">
+                        <h4> Date of Birth: </h4>
+                    </label>
+                    <input type="date" name="dob" required>
+                </div>
+            </div>
+
+            <div class="row">
+                <h4>Gender</h4>
+                <div class="input-group">
+                    <input class="col-half" id="gender-male" type="radio" name="gender" value="Male" required />
+                    <label for="gender-male">Male</label>
+                    <input class="col-half" id="gender-female" type="radio" name="gender" value="Female" required />
+                    <label class="float-right" for="gender-female">Female</label>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-group">
+                    <div class="col-half">
+                        <input type="text" placeholder="Weight" name="weight" required />
+                    </div>
+                    <div class="col-half">
+                        <input type="text" placeholder="Height" name="height" required />
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="input-group">
+                    <h4 class="col-half">Race:</h4>
+                    <select class="float-right" name="race" id="allergies">
+                        <option value="100">White</option>
+                        <option value="101">Black or African American</option>
+                        <option value="102">Asian</option>
+                        <option value="103">Native Hawaiian or Other Pacific Islander</option>
+                        <option value="104">Some other race</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-group">
+                    <h4 class="col-half">Are you allergic to one of the following?</h4>
+                    <select class="float-right" name="allergies" id="allergies">
+                        <option value="200">N/A</option>
+                        <option value="201">Amoxicillin</option>
+                        <option value="202">Aspirin</option>
+                        <option value="203">Insulin</option>
+                        <option value="204">Carbamazepine</option>
+                        <option value="205">Ibuprofen</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="input-group input-group-icon">
+                <input type="text" placeholder="Address" name="address" required />
+                <div class="input-icon"><i class="fa fa-user"></i></div>
+            </div>
+            <div class="input-group">
+                <div class="col-third">
+                    <input type="text" placeholder="City" name="city" required />
+                </div>
+                <div class="col-third">
+                    <input type="text" placeholder="State" name="state" required />
+                </div>
+                <div class="col-third">
+                    <input type="text" placeholder="Zip Code" name="zip" required />
+                </div>
+
+            </div>
+            <input class="button" type="submit" name="update_patient_data">
+        </form>
+    </div>
+
+
+</body>
+
+</html>
