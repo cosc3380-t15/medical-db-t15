7cb85832d1d1336d6dd2e4aec65670ee58d03efd
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-08 20:58:32 -0500

    dr

--- index/pat_profile.php
+++ index/pat_profile.php
@@ -52,7 +52,7 @@
         </a>
         <div class="profile-submenu">
             <a href ="#" class="profile-submenu-item" onclick="load_html('pat_card.php')">View Profile</a>
-            <a href ="#" class="profile-submenu-item" onclick="load_html('/backend/edit_patient.php')">Edit Profile</a>
+            <a href ="href='/backend/edit_patient.php?Pat_ID=".$result['Pat_ID']."'" class="profile-submenu-item" onclick="load_html('/backend/edit_patient.php')">Edit Profile</a>
             
         </div>
 
