0e74e23a57132cde3b49f5d99466f4e4a13c7461
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-08 22:45:38 -0500

    dr

--- index/pat_profile.php
+++ index/pat_profile.php
@@ -54,7 +54,7 @@
         <div class="profile-submenu">
             <a href ="#" class="profile-submenu-item" onclick="load_html('pat_card.php')">View Profile</a>
             <a href ="#" onclick="load_html('/backend/edit_patient_profile.php')" class="profile-submenu-item" >Edit Profile</a>
-            <!-- <a href='/backend/edit_patient.php?Pat_ID="<?php echo".$_SESSION['id'].";  ?>"' class='btn'>Edit</a> -->
+            
        
            
             
