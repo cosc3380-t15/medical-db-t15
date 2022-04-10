69a56387deca646f041fbb47946c476320837a75
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-10 18:26:13 -0500

    dr

--- backend/edit_prescription.php
+++ backend/edit_prescription.php
@@ -18,7 +18,7 @@ if (isset($_POST['update_perscription_data'])) {
 
     if ($query_run) {
         $_SESSION['status'] = "Data Updated Successfully ";
-        header("Location:show_patient.php");
+        header("Location:doc_profile.php");
     }else {
         $_SESSION['status'] = "Not Updated ";
         header("Location:doc_profile.php");
