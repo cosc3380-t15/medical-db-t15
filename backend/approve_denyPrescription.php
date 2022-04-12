6abe23bcb84cfdedbe659c8cfa4a6965940ee95d
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-11 19:56:01 -0500

    dr

--- backend/approve_denyPrescription.php
+++ backend/approve_denyPrescription.php
@@ -34,7 +34,7 @@ I AM SURE THE TABLE IS PRINTED ITS THE APPROVE AND DENY THAT ARE CAUSING PROBLEM
     }
 
     // this is the main query and it works
-    $select="SELECT * FROM prescription WHERE p.Per_Status = 'PENDING' ";  
+    $select="SELECT * FROM prescription WHERE Per_Status = 'PENDING' ";  
     $query=mysqli_query($link,$select);
 ?>    
 <!DOCTYPE html>
