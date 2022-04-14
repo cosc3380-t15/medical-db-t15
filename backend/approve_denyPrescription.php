1db4a2914cdf487a65fbf9579b38f0748ab358cb
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-13 19:47:29 -0500

    dr

--- backend/approve_denyPrescription.php
+++ backend/approve_denyPrescription.php
@@ -43,6 +43,8 @@
 </head>
 
 <body>
+<iframe src="/backend/approve_denyPrescription.php" name="targetframe" allowTransparency="true" scrolling="no" frameborder="0" >
+
      <div class="back-button">
        <a href="/index/home.php" id="back-button" >Back</a> 
     </div> 
@@ -84,5 +86,5 @@
     ?>
     
     </table>
-
+    </iframe>
 </body>
