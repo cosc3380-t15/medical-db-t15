538bbb8261205af8e542f4dd1f797d43216cfe21
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-15 17:22:07 -0500

    dr

--- backend/show_appointments.php
+++ backend/show_appointments.php
@@ -7,7 +7,7 @@
     $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
                                                         
     mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
-
+    
     if (isset($_GET['Appt_ID'])) {  
         $Appt_ID = $_GET['Appt_ID'];  
         $query = "DELETE FROM `appointment` WHERE Appt_ID = '$Appt_ID'";  
@@ -19,9 +19,7 @@
         }  
     }
 
-
-
-    $select="SELECT a.Pat_ID, a.Doc_ID, a.Off_ID, a.Appt_Time, a.Appt_Specialization, p.Pat_First, p.Pat_Last
+    $select="SELECT a.Pat_ID, a.Doc_ID, a.Off_ID, a.Appt_Time, a.Appt_Specialization, p.Pat_First, p.Pat_Last, a.Appt_ID
     FROM appointment AS a, patient AS p
     WHERE p.Pat_ID = a.Pat_ID";
     $query=mysqli_query($link,$select);
@@ -47,6 +45,9 @@
         <th>Appointment Time</th>
         <th>Appointment Specialization</th>
         <th>Doctor ID</th>
+        <th>Appointment ID</th>
+        <th>Opperation</th>
+
     </tr>
     <?php 
         $num=mysqli_num_rows($query);
@@ -61,9 +62,11 @@
                         <td>".$result['Appt_Time']."</td>
                         <td>".$result['Appt_Specialization']."</td>
                         <td>".$result['Doc_ID']."</td>
+                        <td>".$result['Appt_ID']."</td>
                         <td>
                             <a href='/backend/show_appointments_patient.php?Appt_ID=".$result['Appt_ID']."'class='btn'>Cancel</a>
                         </td>
+                        
             
                     </tr>
                 
