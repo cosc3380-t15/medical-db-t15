d53eecc83e2e9c34690fa0fc72e9d884e25205c1
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-15 17:20:07 -0500

    dr

--- backend/show_appointments.php
+++ backend/show_appointments.php
@@ -8,6 +8,19 @@
                                                         
     mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 
+    if (isset($_GET['Appt_ID'])) {  
+        $Appt_ID = $_GET['Appt_ID'];  
+        $query = "DELETE FROM `appointment` WHERE Appt_ID = '$Appt_ID'";  
+        $run = mysqli_query($link,$query);  
+        if ($run) {  
+            header('location:/index/pat_profile.php');  
+        }else{  
+            echo "Error: ".mysqli_error($link);  
+        }  
+    }
+
+
+
     $select="SELECT a.Pat_ID, a.Doc_ID, a.Off_ID, a.Appt_Time, a.Appt_Specialization, p.Pat_First, p.Pat_Last
     FROM appointment AS a, patient AS p
     WHERE p.Pat_ID = a.Pat_ID";
@@ -48,7 +61,9 @@
                         <td>".$result['Appt_Time']."</td>
                         <td>".$result['Appt_Specialization']."</td>
                         <td>".$result['Doc_ID']."</td>
-                        
+                        <td>
+                            <a href='/backend/show_appointments_patient.php?Appt_ID=".$result['Appt_ID']."'class='btn'>Cancel</a>
+                        </td>
             
                     </tr>
                 
