43e4e63a70ed427dd2f787438c5cce577df5ef8a
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-16 00:25:13 -0500

    dr

--- backend/show_patient.php
+++ backend/show_patient.php
@@ -138,7 +138,7 @@
         </form>
         <div>
             <form>
-            <div class="input-group">
+            <div class="row input-group">
                 <input class="button2" type="button" value="BACK" onclick="history.back()">            
                 <input class="button2" type="button" onclick="location.href='/index/OA_profile.php?'" value="Profile" />
                 <input class="button2" type="button" onclick="location.href='/index/home.php?'" value="Home" />
