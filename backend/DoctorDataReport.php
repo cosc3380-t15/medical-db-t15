e1195cd83ed2ceed067e34001a53a83ee9e2fcb4
Muhammad Mamoon Uddin <mamoonuddin17@gmail.com>
2022-04-15 21:35:39 -0500

    Fixed Where preset value causes a space in search

--- backend/DoctorDataReport.php
+++ backend/DoctorDataReport.php
@@ -1,6 +1,5 @@
 <!-- 
     SO THIS FILE IS MAKING A SEARCH FIELD ON THE TOP AND THEN HAS A TABLE LIKE BEFORE ON THE BOTTOM.
-
     REMOVING DATE OF BIRTH SEARCH CURRENTLY CAUSE IDK HOW TO GET THE DATE VALUE OR PRESET ONE
 --> 
 <?php 
@@ -17,7 +16,7 @@
         $query = "DELETE FROM `doctor` WHERE Doc_ID = '$Doc_ID'";  
         $run = mysqli_query($link,$query);  
         if ($run) {  
-            header('location:/backend/approve_denyPrescription.php');  
+            header('location:/index/doc_profile.php');   
         }else{  
             echo "Error: ".mysqli_error($link);  
         }  
@@ -33,9 +32,9 @@
         $Gender = $_GET['gender'];
         // $DOB = $_GET['DOB'];
         // initiallizing as a string this is aproblem cause idk what to do with date types
-        $DOB = " ";
+        $DOB = "";
 
-        if ($ID != ' ' ||$Spec != ' ' ||$FName != ' ' ||$Mname != ' ' ||$Lname != ' ' ||$Gender != ' ' ||$DOB != ' ')
+        if ($ID != '' ||$Spec != '' ||$FName != '' ||$Mname != '' ||$Lname != '' ||$Gender != '' ||$DOB != '')
         {
             // something changed so do this
             $select = "SELECT * FROM doctor WHERE Doc_ID = '$ID' or Doc_Spec LIKE '$Spec' or Doc_Gender = '$Gender' or Doc_First LIKE '$FName' or Doc_M_Init LIKE '$Mname' or Doc_Last LIKE '$Lname'  ";
@@ -90,35 +89,35 @@
             <div>
                 <label > Doctor ID </label>
                 <div >
-                    <input type = "text"  name = "ID" placeholder= "0000000" value = " ">
+                    <input type = "text"  name = "ID" placeholder= "0000000" value = "">
                 </div>
             </div>
             
             <div >
                 <label> Speciality </label>
                 <div>
-                    <input type = "text" name = "Spec" placeholder= "something" value = " ">
+                    <input type = "text" name = "Spec" placeholder= "something" value = "">
                 </div>
             </div>
                        
             <div >
                 <label > First Name </label>
                 <div >
-                    <input type = "text" name = "Fname" placeholder= "First Name" value = " ">
+                    <input type = "text" name = "Fname" placeholder= "First Name" value = "">
                 </div>
             </div>
             
             <div >
                 <label > Middle Initial </label>
                 <div >
-                    <input type = "text"  name = "Mname" placeholder= "Middle initial" value = " ">
+                    <input type = "text"  name = "Mname" placeholder= "Middle initial" value = "">
                 </div>
             </div>
             
             <div >
                 <label > Last Name </label>
                 <div >
-                    <input type = "text" name = "Lname" placeholder= "Last Name" value = " ">
+                    <input type = "text" name = "Lname" placeholder= "Last Name" value = "">
                 </div>
             </div>        
 
@@ -127,7 +126,7 @@
                 <div >
                     <input type ="radio" name="gender" value ="Male"> Male
                     <input type ="radio" name="gender" value ="Female"> Female
-                    <input type ="radio" name="gender" value =" " checked = "checked"> Either
+                    <input type ="radio" name="gender" value ="" checked = "checked"> Either
                 </div>
             </div>
             
@@ -155,16 +154,13 @@
                     <th>ID </th>
                     <th>Speciality </th>
                     <th>First </th>
-                    <th>Middle </th>
+                   
                     <th>Last </th>
                     <th>Email </th>
                     <th>Phone</th>
                     <th>Gender</th>
-                    <!-- <th>Date of Birth </th> -->
-                    <th>Street Address </th>
-                    <th>City Adress </th>
-                    <th>State Address </th>
-                    <th>ZIP Address </th>
+                    <th>Date of Birth </th>
+                   
                     <th> buttons <th>
                 </tr>
             </thead>
@@ -179,16 +175,13 @@
                                 <td>".$result['Doc_ID']." </td>
                                 <td> ".$result['Doc_Spec']." </td>
                                 <td> ".$result['Doc_First']." </td>
-                                <td> ".$result['Doc_M_Init']." </td>
+                                
                                 <td> ".$result['Doc_Last']." </td>
                                 <td> ".$result['Doc_Email']." </td>
                                 <td> ".$result['Doc_Phone']." </td>
                                 <td> ".$result['Doc_Gender']." </td>
                                 <td> ".$result['Doc_DOB']." </td>
-                                <td> ".$result['Doc_Street_Addr']." </td>
-                                <td> ".$result['Doc_City_Addr']." </td>
-                                <td> ".$result['Doc_State_Addr']." </td>
-                                <td> ".$result['Doc_Zip_Addr']." </td>
+                              
                                 <td>
                                 <a href='/backend/DoctorDataReport.php?Per_ID=".$result['Doc_ID']." 'class='deny btn'>DELETE</a>
                                 <a href='/index/OA_profile.php? 'class='btn'>Edit</a>   
