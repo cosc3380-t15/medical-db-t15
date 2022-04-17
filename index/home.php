23fe0b39f14f3ecf4fc35a0cc02c02b2f5b6384d
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-16 21:18:43 -0500

    dr

--- index/home.php
+++ index/home.php
@@ -183,7 +183,6 @@
                 <ul class="pricing-features">
                     <li class="pricing-features-item">Lorem ipsum</li>
                     <li class="pricing-features-item">Lorem ipsum</li>
-                    <li class="pricing-features-item">Lorem ipsum</li>
                     <li class="pricing-features-item">Lorem ipsum</li>  
                     <li class="pricing-features-item">Lorem ipsum</li>
                 </ul>
