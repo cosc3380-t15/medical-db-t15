295fe6e65696d0ced30e4884a0bb195e43e55766
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-18 16:36:06 -0500

    dr

--- backend/create_appointment.php
+++ backend/create_appointment.php
@@ -19,10 +19,9 @@ $query1 = "INSERT INTO appointment (Pat_ID,Doc_ID,Off_ID,Appt_Specialization,App
 VALUES ('$pat_id','$doc_id','$location','$appt_spec','$date','$time')";
 
 
-
-if ($link->query($query1) !== TRUE) {
-    alert("Error message: %s\n", mysqli_error($query1)); 
-} mysqli_close($link);
+if (!$mysqli->query($query1)) {
+    printf("Error message: %s\n", $mysqli->error);
+}
 
 
 header('Location: ../index/pat_profile.php');                 
