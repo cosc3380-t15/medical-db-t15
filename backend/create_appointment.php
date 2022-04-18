ac6bd1e75bf3ce6526172f28fa779362b1e8788d
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-18 16:47:03 -0500

    dr

--- backend/create_appointment.php
+++ backend/create_appointment.php
@@ -1,7 +1,5 @@
 <?php
 
-try {
-    
 $doc_id = $_POST['doc_id'];
 $pat_id = $_POST['pat_id'];
 $location = $_POST['location'];
@@ -20,12 +18,15 @@ mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
 $query1 = "INSERT INTO appointment (Pat_ID,Doc_ID,Off_ID,Appt_Specialization,Appt_Date,Appt_Time)
 VALUES ('$pat_id','$doc_id','$location','$appt_spec','$date','$time')";
 
-header('Location: ../index/pat_profile.php');   
-} catch (Exception $e) {
-    echo 'Caught exception: ',  $e->getMessage(), "\n";
+
+
+
+if (!$link->query($query1)) {
+    printf("Error message: %s\n", $mysqli->error);
 }
 
-              
+
+header('Location: ../index/pat_profile.php');                 
 
 
 ?>
