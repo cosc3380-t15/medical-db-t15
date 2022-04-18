050929b2eae6eeb732ef600c706a8ce699aeaf21
Dragomir Nonov <nonov.dragomir@gmail.com>
2022-04-18 18:26:08 -0500

    dr

--- backend/create_appointment.php
+++ backend/create_appointment.php
@@ -31,6 +31,7 @@ catch(\Exception $e){
     echo '<script language="javascript">';
     echo 'alert("There is an appointment at this time already, please select different time or date.")';
     echo '</script>';
+    sleep(3);
     header('Location: ../index/pat_profile.php');
     // echo 'Query Exception: ' . $e->getMessage(); 
 }
