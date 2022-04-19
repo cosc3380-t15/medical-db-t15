83dc6279ce76b9d9fc011829df4553c1e1a26502
Michael Summers <msasummers@yahoo.com>
2022-04-19 17:59:15 -0500

    triggers

--- backend/dumps/Dump20220419.sql
+++ backend/dumps/Dump20220419.sql
@@ -81,6 +81,87 @@ LOCK TABLES `appointment` WRITE;
 INSERT INTO `appointment` VALUES (160,1012249,2000014,'Houston','Dentist','2022-04-19','14:00:00'),(161,1012249,2000014,'Houston','Dentist','2022-04-19','13:00:00'),(163,1012248,2000014,'Houston','Dentist','2022-04-19','11:00:00');
 /*!40000 ALTER TABLE `appointment` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`DNonov`@`%`*/ /*!50003 TRIGGER `repeating_appt` BEFORE INSERT ON `appointment` FOR EACH ROW BEGIN
+   IF (EXISTS(SELECT 1 
+	   FROM appointment 
+	   WHERE Doc_ID = new.Doc_ID 
+	   AND Appt_Time = new.Appt_Time
+       AND Appt_Date = new.Appt_Date)
+	   )THEN 
+		   SIGNAL SQLSTATE VALUE '45000' 
+		   SET MESSAGE_TEXT = 'There is an appointment at this time already, please chose another time or date.';
+   END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`DNonov`@`%`*/ /*!50003 TRIGGER `max_capcity2` BEFORE INSERT ON `appointment` FOR EACH ROW BEGIN
+    DECLARE count INT;
+    SELECT COUNT(Appt_Time)
+    INTO count
+    FROM appointment
+    WHERE new.Appt_Date = Appt_Date AND new.Off_ID = Off_ID;
+
+    IF count >= 3 THEN
+		UPDATE max_capacity
+		SET max_capacity_reaching = 'TRUE'
+		WHERE location = new.Off_ID;
+    END IF;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`DNonov`@`%`*/ /*!50003 TRIGGER `max_capcity` AFTER DELETE ON `appointment` FOR EACH ROW BEGIN
+    DECLARE count INT;
+    SELECT COUNT(Appt_Time)
+    INTO count
+    FROM appointment
+    WHERE old.Appt_Date = Appt_Date AND old.Off_ID = Off_ID;
+
+    IF count <= 3 THEN
+		UPDATE max_capacity
+		SET max_capacity_reaching = 'FALSE'
+		WHERE location = old.Off_ID;
+    END IF;
+
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `doctor`
@@ -212,6 +293,14 @@ LOCK TABLES `prescription` WRITE;
 INSERT INTO `prescription` VALUES (30,1012239,2000000,'Aspirin','APPROVED'),(31,1012247,2000000,'Amoxicillin','DENIED'),(32,1012240,2000000,'Amoxicillin','APPROVED'),(33,1012249,2000024,'Amoxicillin','APPROVED'),(34,1012249,2000024,'Aspirin','PENDING'),(35,1012239,2000014,'Amoxicillin','PENDING'),(36,1012239,2000014,'Insulin','APPROVED');
 /*!40000 ALTER TABLE `prescription` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'medical_db'
+--
+
+--
+-- Dumping routines for database 'medical_db'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -222,4 +311,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-19 17:52:13
+-- Dump completed on 2022-04-19 17:55:36
