a87907c962c598febb142dcce17805598257989e
Muhammad Mamoon Uddin <mamoonuddin17@gmail.com>
2022-04-23 22:23:14 -0500

    fixing admin profile bug

--- backend/edit_admin_profile.php
+++ backend/edit_admin_profile.php
@@ -10,7 +10,6 @@ $dbhost = "eyiece.mynetgear.com";
   $dbpass = "93U#muq!fPzZ"; 
   $dbname = "medical_db";
 
-  $_SESSION['id'] = 3000000;
 $link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
                                                     
 mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
